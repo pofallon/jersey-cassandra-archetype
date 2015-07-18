061f2829d14056275f06d10069b26459fe229a16
Paul O'Fallon <paul@ofallonfamily.com>
2015-07-18 10:25:02 -0400

    Enhanced default cluster service

--- src/main/resources/archetype-resources/src/main/java/ClusterService.java
+++ src/main/resources/archetype-resources/src/main/java/ClusterService.java
@@ -5,9 +5,13 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+
 import java.util.HashMap;
+import java.util.HashSet;
 
 import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Host;
+import com.datastax.driver.core.KeyspaceMetadata;
 import com.datastax.driver.core.Metadata;
 
 @Path("/cluster")
@@ -16,13 +20,41 @@ public class ClusterService {
     @Context Cluster cluster;
 
     @GET
-    @Path("/name")
     @Produces(MediaType.APPLICATION_JSON)
-    public String getClusterName() {
+    public HashMap<String,HashMap<String,Object>> getClusterInfo() {
 
         Metadata metadata = cluster.getMetadata();
-        return(metadata.getClusterName());
+        HashMap<String,Object> clusterInfo = new HashMap<String,Object>();
 
+        HashSet<HashMap<String,Object>> hosts = new HashSet<HashMap<String,Object>>();
+        for (Host h : metadata.getAllHosts()) {
+            HashMap<String,Object> hostMap = new HashMap<String,Object>();
+
+            hostMap.put("datacenter",h.getDatacenter());
+            hostMap.put("socketAddress",h.getSocketAddress());
+            hostMap.put("rack",h.getRack());
+            hostMap.put("state",h.getState());
+            hostMap.put("tokenCount",h.getTokens().size());
+            hosts.add(hostMap);
+
+        }
+        clusterInfo.put("hosts",hosts);
+
+        HashMap<String,HashMap<String,Object>> keyspaces = new HashMap<String,HashMap<String,Object>>();
+        for (KeyspaceMetadata k : metadata.getKeyspaces()) {
+            HashMap<String,Object> keyspaceMap = new HashMap<String,Object>();
+            keyspaceMap.put("replication",k.getReplication());
+            keyspaceMap.put("tableCount",k.getTables().size());
+            keyspaceMap.put("userTypeCount",k.getUserTypes().size());
+            keyspaces.put(k.getName(),keyspaceMap);
+
+        }
+        clusterInfo.put("keyspaces",keyspaces);
+
+        HashMap<String,HashMap<String,Object>> results = new HashMap<String,HashMap<String,Object>>();
+        results.put(metadata.getClusterName(),clusterInfo);
+
+        return(results);
     }
 
 }
