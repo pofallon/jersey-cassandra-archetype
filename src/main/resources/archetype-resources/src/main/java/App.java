42d5af157087279a2527dcd751a232a26fe4b1f4
Paul OFallon <paul@Pauls-Mac-mini.local>
2015-07-17 20:01:58 -0400

    Initial check-in

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.ofallonfamily.archetypes;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
